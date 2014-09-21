f492abacbd3106b80ff4691d3dc71ce4a2696b26
James Bolding <bolwire@cableone.net>
2014-09-21 16:24:59 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace RenardTester
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+			this.comboBoxPort = new System.Windows.Forms.ComboBox();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.buttonStop = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.buttonAllOn = new System.Windows.Forms.Button();
+			this.buttonAllOff = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.CheckOnClick = true;
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(144, 439);
+			this.checkedListBox1.TabIndex = 0;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(12, 499);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            64,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(144, 20);
+			this.numericUpDown1.TabIndex = 1;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+			this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 483);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Channel Count";
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(162, 28);
+			this.trackBar1.Maximum = 255;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+			this.trackBar1.Size = new System.Drawing.Size(45, 437);
+			this.trackBar1.TabIndex = 3;
+			this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+			this.trackBar1.Value = 255;
+			this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(162, 468);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(21, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "0%";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(159, 12);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(33, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "100%";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(213, 28);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(121, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 50;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(210, 91);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(58, 13);
+			this.label4.TabIndex = 12;
+			this.label4.Text = "Baud Rate";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(210, 51);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(50, 13);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "Com Port";
+			// 
+			// comboBoxBaudRate
+			// 
+			this.comboBoxBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxBaudRate.FormattingEnabled = true;
+			this.comboBoxBaudRate.Location = new System.Drawing.Point(213, 107);
+			this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+			this.comboBoxBaudRate.Size = new System.Drawing.Size(121, 21);
+			this.comboBoxBaudRate.TabIndex = 10;
+			// 
+			// comboBoxPort
+			// 
+			this.comboBoxPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxPort.FormattingEnabled = true;
+			this.comboBoxPort.Location = new System.Drawing.Point(213, 67);
+			this.comboBoxPort.Name = "comboBoxPort";
+			this.comboBoxPort.Size = new System.Drawing.Size(121, 21);
+			this.comboBoxPort.TabIndex = 9;
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Location = new System.Drawing.Point(213, 134);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(56, 23);
+			this.buttonStart.TabIndex = 13;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// buttonStop
+			// 
+			this.buttonStop.Location = new System.Drawing.Point(278, 134);
+			this.buttonStop.Name = "buttonStop";
+			this.buttonStop.Size = new System.Drawing.Size(56, 23);
+			this.buttonStop.TabIndex = 13;
+			this.buttonStop.Text = "Stop";
+			this.buttonStop.UseVisualStyleBackColor = true;
+			this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(210, 12);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(76, 13);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Intensity Value";
+			// 
+			// buttonAllOn
+			// 
+			this.buttonAllOn.Location = new System.Drawing.Point(12, 457);
+			this.buttonAllOn.Name = "buttonAllOn";
+			this.buttonAllOn.Size = new System.Drawing.Size(56, 23);
+			this.buttonAllOn.TabIndex = 13;
+			this.buttonAllOn.Text = "All On";
+			this.buttonAllOn.UseVisualStyleBackColor = true;
+			this.buttonAllOn.Click += new System.EventHandler(this.buttonAllOn_Click);
+			// 
+			// buttonAllOff
+			// 
+			this.buttonAllOff.Location = new System.Drawing.Point(100, 457);
+			this.buttonAllOff.Name = "buttonAllOff";
+			this.buttonAllOff.Size = new System.Drawing.Size(56, 23);
+			this.buttonAllOff.TabIndex = 13;
+			this.buttonAllOff.Text = "All Off";
+			this.buttonAllOff.UseVisualStyleBackColor = true;
+			this.buttonAllOff.Click += new System.EventHandler(this.buttonAllOff_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(339, 531);
+			this.Controls.Add(this.buttonAllOff);
+			this.Controls.Add(this.buttonStop);
+			this.Controls.Add(this.buttonAllOn);
+			this.Controls.Add(this.buttonStart);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.comboBoxBaudRate);
+			this.Controls.Add(this.comboBoxPort);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.checkedListBox1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Renard Tester";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ComboBox comboBoxBaudRate;
+		private System.Windows.Forms.ComboBox comboBoxPort;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Button buttonStop;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button buttonAllOn;
+		private System.Windows.Forms.Button buttonAllOff;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace RenardTester
+{
+	public partial class Form1 : Form
+	{
+
+		private readonly object[] _baudRates = { "19200", "38400", "57600", "76800", "115200", "230400", "460800" };
+		private SerialPort _port;
+		private String[] _myPacket;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			UpdateChannelList((int)numericUpDown1.Value);
+		}
+
+		private void UpdateChannelList(int channelCount)
+		{
+			checkedListBox1.BeginUpdate();
+			checkedListBox1.Items.Clear();
+
+			for (int i = 1; i <= channelCount; i++)
+			{
+				checkedListBox1.Items.Add("Channel " + i);
+			}
+
+			checkedListBox1.EndUpdate();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			UpdateChannelList((int)numericUpDown1.Value);
+			comboBoxPort.Items.AddRange(items: SerialPort.GetPortNames());
+			comboBoxBaudRate.Items.AddRange(_baudRates);
+			textBox1.Text = trackBar1.Value.ToString("X");
+		}
+
+		private void trackBar1_ValueChanged(object sender, EventArgs e)
+		{
+			textBox1.Text = trackBar1.Value.ToString("X");
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			if (_port != null && _port.IsOpen || comboBoxPort.Text == "" || comboBoxBaudRate.Text == "")
+				return;
+
+			_port = new SerialPort(comboBoxPort.Text, Convert.ToInt32(comboBoxBaudRate.Text), Parity.None, Convert.ToInt32(8),
+				StopBits.One);
+
+
+			try
+			{
+				_port.Handshake = Handshake.None;
+				_port.Encoding = Encoding.UTF8;
+				_port.RtsEnable = true;
+				_port.DtrEnable = true;
+				_port.Open();
+				timer1.Enabled = true;
+
+			}
+
+			catch (IOException)
+			{
+				MessageBox.Show(String.Format("COM port: {0} could not be accessed.", _port.PortName), @"Write failed, IO Exception",
+					MessageBoxButtons.OK);
+
+			}
+		}
+
+		private void buttonStop_Click(object sender, EventArgs e)
+		{
+			if (_port == null)
+				return;
+
+			if (_port.IsOpen)
+			{
+				_myPacket = new String[(int)numericUpDown1.Value + 2];
+
+				_myPacket[0] = "7E";
+				_myPacket[1] = "80";
+
+				for (int i = 0; i <= checkedListBox1.Items.Count - 1; i++)
+				{
+					_myPacket[i + 2] = "00";
+				}
+
+				byte[] myPacket = _myPacket.Select(s => Convert.ToByte(s, 16)).ToArray();
+
+				_port.Write(myPacket, 0, myPacket.Length);
+				_port.Close();
+				_port.Dispose();
+				timer1.Enabled = false;
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (!_port.IsOpen)
+				return;
+
+			_myPacket = new String[(int)numericUpDown1.Value + 2];
+
+			_myPacket[0] = "7E";
+			_myPacket[1] = "80";
+
+			for (int i = 0; i <= checkedListBox1.Items.Count - 1; i++)
+			{
+				_myPacket[i + 2] = checkedListBox1.CheckedIndices.Contains(i) ? trackBar1.Value.ToString("X") : "00";
+			}
+
+			byte[] myPacket = _myPacket.Select(s => Convert.ToByte(s, 16)).ToArray();
+
+			_port.Write(myPacket, 0, myPacket.Length);
+		}
+
+		private void buttonAllOn_Click(object sender, EventArgs e)
+		{
+			SetAllChecks(true);
+		}
+
+		private void buttonAllOff_Click(object sender, EventArgs e)
+		{
+			SetAllChecks(false);
+		}
+
+		private void SetAllChecks(bool checkState)
+		{
+			for (int i = 0; i <= (checkedListBox1.Items.Count - 1); i++)
+			{
+				checkedListBox1.SetItemCheckState(i, checkState ? CheckState.Checked : CheckState.Unchecked);
+			} 
+		}
+
+
+	}
+}
